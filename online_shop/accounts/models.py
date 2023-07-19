12f8daf560458660af176a08ee9e9cee0e4d44d3
kiana alamy <as.faraso.97@gmail.com>
2023-07-19 11:17:12 +0430

    start OTPcode in models

--- online_shop/accounts/models.py
+++ online_shop/accounts/models.py
@@ -22,3 +22,11 @@ class User(AbstractBaseUser, PermissionsMixin):
 	def is_staff(self):
 		return self.is_admin
 
+
+class OtpCode(models.Model):
+	phone_number = models.CharField(max_length=11, unique=True)
+	code = models.PositiveSmallIntegerField()
+	created = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return f'{self.phone_number} - {self.code} - {self.created}'
