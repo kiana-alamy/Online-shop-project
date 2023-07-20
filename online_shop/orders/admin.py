2e2ffdf93e49ebbe88fa44a428bfbd9361a8796b
kiana alamy <as.faraso.97@gmail.com>
2023-07-20 07:45:45 +0430

    OrderItemInline class in orders.admin.py

--- online_shop/orders/admin.py
+++ online_shop/orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+	model = OrderItem
+	raw_id_fields = ('product',)
