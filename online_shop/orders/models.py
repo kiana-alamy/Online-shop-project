cd7f43a9c2f31f4035f39b2e6ebf458e849d0058
kiana alamy <as.faraso.97@gmail.com>
2023-07-24 16:10:48 +0430

    Remove home dependencies

--- online_shop/orders/models.py
+++ online_shop/orders/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from home.models import Product
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
@@ -27,7 +26,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
 	order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='item_s')
-	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	# product = models.ForeignKey(Product, on_delete=models.CASCADE)
 	price = models.IntegerField()
 	quantity = models.IntegerField(default=1)
 
