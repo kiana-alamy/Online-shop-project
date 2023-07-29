72e784f6ba35436692c8ca1bb54dd0f3ee9050df
kiana alamy <as.faraso.97@gmail.com>
2023-07-29 14:48:01 +0430

    create UserAdmin class in accounts.admin

--- online_shop/accounts/admin.py
+++ online_shop/accounts/admin.py
@@ -1,32 +1,29 @@
-# from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin
-# from django.contrib.auth.models import Group
-# from django.utils.translation import gettext_lazy as _
-# from .models import User
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# class CustomUserAdmin(UserAdmin):
-#     model = User
-#     list_display = ['phone_number', 'full_name', 'is_admin']
-#     list_filter = ['is_admin', 'is_active']
-#     fieldsets = (
-#         (None, {'fields': ('phone_number', 'password')}),
-#         (_('Personal Info'), {'fields': ('full_name', 'email')}),
-#         (_('Permissions'), {'fields': ('is_admin', 'is_active', 'groups', 'user_permissions')}),
-#         (_('Important dates'), {'fields': ('last_login',)}),
-#     )
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('phone_number', 'full_name', 'email', 'password1', 'password2'),
-#         }),
-#     )
-#     search_fields = ['phone_number', 'full_name', 'email']
-#     ordering = ['id']
-#     filter_horizontal = ('groups', 'user_permissions')
-#     empty_value_display = '-empty-'
+class UserAdmin(UserAdmin):
+    model = User
+    list_display = ['phone_number', 'full_name', 'is_admin']
+    list_filter = ['is_admin', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        (_('Personal Info'), {'fields': ('full_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_admin', 'is_active', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('phone_number', 'full_name', 'email', 'password1', 'password2'),}),
+    )
+    search_fields = ['phone_number', 'full_name', 'email']
+    ordering = ['id']
+    filter_horizontal = ('groups', 'user_permissions')
+    # empty_value_display = '-empty-'
 
-# admin.site.register(User, CustomUserAdmin)
-# admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
 
 
 # class OtpCodeAdmin(admin.ModelAdmin):
@@ -46,8 +43,8 @@
 # admin.site.register(Address, AddressAdmin)
 # ************************************
 
-from django.contrib import admin
+# from django.contrib import admin
 
-from .models import User
+# from .models import User
 
-admin.site.register(User)
+# admin.site.register(User)
