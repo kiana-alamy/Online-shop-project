5d8c881450ea08cd5593a07aa1383a07569a280d
kiana alamy <as.faraso.97@gmail.com>
2023-07-31 18:11:17 +0430

    add UserRegisterVerifyCodeView path in urls.py

--- online_shop/accounts/urls.py
+++ online_shop/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('verify/', views.UserRegisterVerifyCodeView.as_view(), name='verify'),
 ]
 
