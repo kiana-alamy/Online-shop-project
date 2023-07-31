a16a7074949a2755ce7452424f49c4d1224f2d91
kiana alamy <as.faraso.97@gmail.com>
2023-07-31 19:06:36 +0430

    Solving admin panel problems

--- online_shop/accounts/admin.py
+++ online_shop/accounts/admin.py
@@ -7,24 +7,33 @@ from .forms import UserChangeForm, UserCreationForm
 
 
 class UserAdmin(BaseUserAdmin):
-    form= UserChangeForm
-    add_form= UserCreationForm
-    list_display = ('phone_number', 'full_name', 'is_admin')
-    list_filter = ('is_admin',)
-    fieldsets = (
-        (None, {'fields': ('phone_number', 'password')}),
-        # (_('Personal Info'), {'fields': ('full_name', 'email')}),
-        # (_('Permissions'), {'fields': ('is_admin', 'is_active', 'groups', 'user_permissions')}),
-        # (_('Important dates'), {'fields': ('last_login',)}),
-    )
-    add_fieldsets = (
-        (None, {'fields': ('phone_number', 'full_name', 'email', 'password1', 'password2'),}),
-    )
-    search_fields = ('phone_number', 'full_name', 'email')
-    ordering = ('full_name',)
-    # filter_horizontal = ('groups', 'user_permissions')
-    filter_horizontal = ()
-    # empty_value_display = '-empty-'
+	form = UserChangeForm
+	add_form = UserCreationForm
+
+	list_display = ('email', 'phone_number', 'is_admin')
+	list_filter = ('is_admin',)
+	readonly_fields = ('last_login',)
+
+	fieldsets = (
+		('Main', {'fields':('email', 'phone_number', 'full_name', 'password')}),
+		('Permissions', {'fields':('is_active', 'is_admin', 'is_superuser', 'last_login', 'groups', 'user_permissions')}),
+	)
+
+	add_fieldsets = (
+		(None, {'fields':('phone_number', 'email', 'full_name', 'password1', 'password2')}),
+	)
+
+	search_fields = ('email', 'full_name')
+	ordering = ('full_name',)
+	filter_horizontal = ('groups', 'user_permissions')
+
+	def get_form(self, request, obj=None, **kwargs):
+		form = super().get_form(request, obj, **kwargs)
+		is_superuser = request.user.is_superuser
+		if not is_superuser:
+			form.base_fields['is_superuser'].disabled = True
+		return form
+    
 admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
 
--- online_shop/accounts/models.py
+++ online_shop/accounts/models.py
@@ -1,37 +1,7 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from core.models import BaseModel
-
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, phone_number, full_name, password=None):
-        if not email:
-            raise ValueError('Users must have an email address')
-        if not phone_number:
-            raise ValueError('Users must have a phone number')
-        if not full_name:
-            raise ValueError('Users must have a full name')
-
-        user = self.model(
-            email=self.normalize_email(email),
-            phone_number=phone_number,
-            full_name=full_name,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, phone_number, full_name, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            phone_number=phone_number,
-            full_name=full_name,
-            password=password,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -41,7 +11,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=True)
 
-    objects = CustomUserManager()
+    objects = UserManager()
 
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS = ['email', 'full_name']
@@ -50,8 +20,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     # user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_permissions', blank=True)
 
     def __str__(self):
-        # return self.email
-        return self.phone_number
+        return self.email
+        # return self.phone_number
 
     @property
     def is_staff(self):
