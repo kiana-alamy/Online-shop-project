d25388c962a85ac507ab694c7e95df1227c6ebfd
kiana alamy <as.faraso.97@gmail.com>
2023-08-01 09:44:03 +0430

    Activate Kavenagar

--- online_shop/utils.py
+++ online_shop/utils.py
@@ -1,3 +1,17 @@
+from kavenegar import *
+from django.conf import settings
 
-def send_otp_code(phone_number , code):
-    pass
+def send_otp_code(phone_number, code):
+    try:
+        kavenegar_instance = KavenegarAPI(settings.API_CODE)
+        params = {
+            'sender': '',
+            'receptor': phone_number, 
+            'message': f'{code} Your verification code',
+        }
+        response = kavenegar_instance.sms_send(params)
+        print(response)
+    except APIException as e:
+        print(e)
+    except HTTPException as e:
+        print(e)
