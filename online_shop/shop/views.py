62d7c31bbe8442a99486c556504b411e8ddfe867
kiana alamy <as.faraso.97@gmail.com>
2023-08-01 13:56:29 +0430

    write a simple static.style.css for home template

--- online_shop/shop/views.py
+++ online_shop/shop/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.views import View
+# from django.shortcuts import get_object_or_404
+# from .models import Product
+# from order.forms import CartAddForm
 
-# Create your views here.
+
+# class ProductDetailView(View):
+#     def get(self, request, slug):
+#         product = Product.objects.get(slug = slug)
+#         # comment = Comment.objects.filter(product_id = product.id)
+#         form = CartAddForm()
+#         return render(request, 'product/detail.html', {'product': product , 'form':form , 'comment':comment})
