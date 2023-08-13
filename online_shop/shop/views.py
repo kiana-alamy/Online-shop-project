e5d6febf6e30968e3ff9507fa39cc6192943f3e2
kiana alamy <as.faraso.97@gmail.com>
2023-08-13 17:37:02 +0430

    add form in ProductDetailView

--- online_shop/shop/views.py
+++ online_shop/shop/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.views import View
 from django.shortcuts import get_object_or_404
 from .models import Product
+from orders.forms import CartAddForm
 
 
 class ProductDetailView(View):
     def get(self, request, slug):
         product = Product.objects.get(slug = slug)
-        print(1111111111111111111111111111111111111111111111111111111)
 
         # comment = Comment.objects.filter(product_id = product.id)
-        # form = CartAddForm()
+        form = CartAddForm()
         # return render(request, 'product/detail.html', {'product': product , 'form':form , 'comment':comment})
-        return render(request, 'shop/detail.html', {'product': product ,})
+        return render(request, 'shop/detail.html', {'product': product , 'form': form})
