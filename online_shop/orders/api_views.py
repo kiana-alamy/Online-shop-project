4356100f86d7bd4410e114dfe10e23d840ffd6ea
kiana alamy <as.faraso.97@gmail.com>
2023-08-22 18:10:17 +0430

    edit orders.api_views

--- online_shop/orders/api_views.py
+++ online_shop/orders/api_views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from accounts.models import Account
+from accounts.models import User
 from .models import Order, OrderItem
 from .serializers import OrderSerializer, OrderItemsSerializer, ItemSerializer
 from shop.models import Product, Category
@@ -23,7 +23,7 @@ class OrderApiView(APIView):
         return Response()
 
     def post(self, request):
-        account = Account.objects.get(id=request.user.id) 
+        account = User.objects.get(id=request.user.id) 
         # orderitem = OrderItem.objects.create(user=account)
         self.check_object_permissions(request ,account)
         srz_data = Order(instance = account,data=request.POST, many=True)
