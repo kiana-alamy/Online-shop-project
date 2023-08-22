65f530d815b4ac1e06c43c9b5a02b77f94ab742a
kiana alamy <as.faraso.97@gmail.com>
2023-08-22 11:23:42 +0430

    add rest_framework path to root urls

--- online_shop/online_shop/urls.py
+++ online_shop/online_shop/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace= 'accounts')),
     path('shop/', include('shop.urls', namespace= 'shop')),
     path('', include('dashboard.urls', namespace= 'home')),
-    path('orders/' , include('orders.urls', namespace='orders'))
+    path('orders/' , include('orders.urls', namespace='orders')),
+    path('api-auth/', include('rest_framework.urls'))
 ]+ static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT) #برای نشون دادن عکس ها در برنامه
+
