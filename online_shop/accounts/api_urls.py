f89c377595d68cffa93c38f41ee7c47a97bcb17f
kiana alamy <as.faraso.97@gmail.com>
2023-08-22 18:14:06 +0430

    edit api_urls and api_views in accounts app

--- online_shop/accounts/api_urls.py
+++ online_shop/accounts/api_urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .api_views import Register
-from rest_framework.authtoken import views as token
+from . import api_views
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name= 'accounts_api'
 urlpatterns = [
-    path('register/',Register.as_view() , name='register'),
-    path('api-token-auth/' , token.obtain_auth_token),
+	path('register/', api_views.UserRegister.as_view()),
+	path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+router = routers.SimpleRouter()
+router.register('users', api_views.UserViewSet)
+urlpatterns += router.urls
--- online_shop/accounts/api_views.py
+++ online_shop/accounts/api_views.py
@@ -1,44 +1,63 @@
-from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
-from .serializers import RegisterSerializers, VerifySerializers, ProfileSerializer
+from rest_framework.response import Response
+from .serializers import UserRegisterSerializer, UserSerializer
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .models import Account, OtpCode
-# from .permissions import IsOwnerOrReadOnly
+from .models import User, OtpCode
+from django.shortcuts import get_object_or_404
 from utils import send_otp_code
 import random
 
 
-class Register(APIView):
-    def post(self, request):
-        ser_data = RegisterSerializers(data=request.POST)
-        if ser_data.is_valid():
-            cd = ser_data.validated_data
-            rand = random.randint(1000, 9999)
-            send_otp_code(cd['phone_number'], rand)
-            OtpCode.objects.create(phone_number=cd['phone_number'], code=rand)
-            request.session['user_registration_info'] = {
+class UserRegister(APIView):
+	def post(self, request):
+		ser_data = UserRegisterSerializer(data=request.POST)
+		if ser_data.is_valid():
+			# ser_data.create(ser_data.validated_data)
+			cd = ser_data.validated_data
+			rand = random.randint(1000, 9999)
+			send_otp_code(cd['phone_number'], rand)
+			OtpCode.objects.create(phone_number=cd ['phone_number'], code=rand)
+			request.session['user_registration_info'] = {
                 'phone_number': cd['phone_number'],
                 'firstname': cd['firstname'],
                 'lastname': cd['lastname'],
                 'password': cd['password']
             }
-            # ser_data.create(ser_data.validated_data)
-            return Response(ser_data.data, status=status.HTTP_201_CREATED)
-        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+			return Response(ser_data.data, status=status.HTTP_201_CREATED)
+		return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class VerifyCodeView(APIView):
-    def post(self, request):
-        user_session = request.session['user_registration_info']
-        code_instance = OtpCode.objects.get(
-            phone_number=user_session['phone_number'])
-        ser_data = VerifySerializers(data=request.POST)
-        if ser_data.is_valid():
-            cd = ser_data.validated_data
-            if code_instance.code == cd['code']:
-                Account.objects.create_user(
-                    phone_number=user_session['phone_number'], firstname=user_session['firstname'], lastname=user_session['lastname'], password=user_session['password'])
-                code_instance.delete()
-                return Response(ser_data.data, status=status.HTTP_201_CREATED)
-            return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserViewSet(viewsets.ViewSet):
+	User = get_user_model()
+	permission_classes = [IsAuthenticated,]
+	queryset = User.objects.all()
+
+	def list(self, request):
+		srz_data = UserSerializer(instance=self.queryset, many=True)
+		return Response(data=srz_data.data)
+
+	def retrieve(self, request, pk=None):
+		user = get_object_or_404(self.queryset, pk=pk)
+		srz_data = UserSerializer(instance=user)
+		return Response(data=srz_data.data)
+
+	def partial_update(self, request, pk=None):
+		user = get_object_or_404(self.queryset, pk=pk)
+		if user != request.user:
+			return Response({'permission denied': 'you are not the owner'})
+		srz_data = UserSerializer(instance=user, data=request.POST, partial=True)
+		if srz_data.is_valid():
+			srz_data.save()
+			return Response(data=srz_data.data)
+		return Response(data=srz_data.errors)
+
+	def destroy(self, request, pk=None):
+		user = get_object_or_404(self.queryset, pk=pk)
+		if user != request.user:
+			return Response({'permission denied': 'you are not the owner'})
+		user.is_active = False
+		user.save()
+		return Response({'message':'user deactivated'})
