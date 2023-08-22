b4133014af310f9fa91ff734232128a6a7ceca5a
kiana alamy <as.faraso.97@gmail.com>
2023-08-22 13:30:34 +0430

    write api_urls for shop app

---
+++ online_shop/shop/api_urls.py
@@ -0,0 +1,9 @@
+from django.urls import path , include
+from .api_views import CategoryListView , ProductListView
+
+app_name ='api_product'
+
+urlpatterns=[
+    path('category/',CategoryListView.as_view() , name='api_category'),
+    path('product/',ProductListView.as_view() , name='api_product'),
+]
