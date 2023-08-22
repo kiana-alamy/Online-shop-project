324478b427ae249996056bb7e0807cff2c82170f
kiana alamy <as.faraso.97@gmail.com>
2023-08-22 18:13:07 +0430

    rewrite better serializer for accounts

--- online_shop/accounts/serializers.py
+++ online_shop/accounts/serializers.py
@@ -1,28 +1,42 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class RegisterSerializers(serializers.ModelSerializer):
-    password2 = serializers.CharField(required=True, write_only=True)
 
-    class Meta:
-        model = User
-        fields = ('phone_number', 'firstname',
-                  'lastname', 'password', 'password2')
-        extra_kwargs = {
-            'password': {'write_only': True},
-        }
+def clean_email(value):
+	if 'admin' in value:
+		raise serializers.ValidationError('admin cant be in email')
 
-    def create(self, validated_data):
-        del validated_data['password2']
-        return User.objects.create_user(**validated_data)
+class UserRegisterSerializer(serializers.ModelSerializer):
+	password2 = serializers.CharField(write_only=True, required=True)
 
-    def validate_username(self, value):
-        if value == 'admin':
-            raise serializers.ValidationError('user cant be admin')
-        return value
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password', 'password2')
+		extra_kwargs = {
+			'password': {'write_only':True},
+			'email': {'validators': (clean_email,)}
+		}
 
-    def validate(self, data):
-        if data['password'] != data['password2']:
-            raise serializers.ValidationError('password must match')
-        return data
+	def create(self, validated_data):
+		del validated_data['password2']
+		return User.objects.create_user(**validated_data)
+
+	def validate_username(self, value):
+		if value == 'admin':
+			raise serializers.ValidationError('username cant be `admin`')
+		return value
+
+	def validate(self, data):
+		if data['password'] != data['password2']:
+			raise serializers.ValidationError('passwords must match')
+		return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = '__all__'
