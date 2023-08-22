0aadeb86535037370db804fc003fea9cdb84cc45
kiana alamy <as.faraso.97@gmail.com>
2023-08-22 13:22:43 +0430

    write api_urls for accounts app

---
+++ online_shop/accounts/api_urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .api_views import Register
+from rest_framework.authtoken import views as token
+
+app_name= 'api'
+urlpatterns = [
+    path('register/',Register.as_view() , name='register'),
+    path('api-token-auth/' , token.obtain_auth_token),
+]
