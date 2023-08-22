03c72d77ced15cd71ba78e5fb9dfd7f2793f8677
kiana alamy <as.faraso.97@gmail.com>
2023-08-22 20:15:54 +0430

    create form in dashboard for search

--- online_shop/dashboard/views.py
+++ online_shop/dashboard/views.py
@@ -8,13 +8,13 @@ class HomeView(View):
     def get(self, request , category_slug=None):
         products = Product.objects.filter(available=True)
         categories = Category.objects.filter( is_sub= False)
-
         paginator = Paginator(products , 2)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         if category_slug:
             category = Category.objects.get(slug = category_slug)
             products = products.filter(category = category)
+        
         return render(request, 'base.html', {'products': page_obj , 'categories':categories})
 
 
