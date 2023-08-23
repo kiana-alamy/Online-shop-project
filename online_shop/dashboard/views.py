bf827cd40f454766ca91a1b5117ec2928c67a2e6
kiana alamy <as.faraso.97@gmail.com>
2023-08-23 16:06:53 +0430

    search , search by category and pagination are done

--- online_shop/dashboard/views.py
+++ online_shop/dashboard/views.py
@@ -6,17 +6,43 @@ from .forms import SearchForm
 
 
 class HomeView(View):
+
     def get(self, request , category_slug=None):
-        products = Product.objects.filter(available=True)
+
+        p = Product.objects.all()
+        products = Product.objects.all()
         categories = Category.objects.filter( is_sub= False)
-        paginator = Paginator(products , 2)
+
+        paginator = Paginator(p , 4)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
+        products = page_obj
+
         form = SearchForm()
+        if 'search' in request.GET:
+            products = Product.objects.all()
+            form= SearchForm(request.GET)
+            if form.is_valid():
+                cd = form.cleaned_data['search']
+                products = products.filter(title__icontains = cd)
+
         if category_slug:
+            products = Product.objects.all()
             category = Category.objects.get(slug = category_slug)
             products = products.filter(category = category)
-        return render(request, 'base.html', {'products': page_obj , 'categories':categories , 'form': form})
+            
+        return render(request, 'base.html', {'product': page_obj ,'products': products , 'categories':categories , 'form': form })
+    
+
+    # def get(self, request):
+
+    #     p = Product.objects.all()
+
+    #     paginator = Paginator(p , 1)
+    #     page_number = request.GET.get('page')
+    #     page_obj = paginator.get_page(page_number)
+
+    #     return render(request, 'shop/product.html', {'product': page_obj })
 
 
 
