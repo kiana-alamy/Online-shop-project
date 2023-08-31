0a8b432a0c95a6b3fabd8da1bc1f0710f5cbe77f
kiana alamy <as.faraso.97@gmail.com>
2023-08-31 13:04:51 +0430

    I arranged the order of searches in the file dashboard.views

--- online_shop/dashboard/views.py
+++ online_shop/dashboard/views.py
@@ -4,45 +4,25 @@ from shop.models import Product , Category
 from django.core.paginator import Paginator
 from .forms import SearchForm
 
-
 class HomeView(View):
-
     def get(self, request , category_slug=None):
-
-        p = Product.objects.all()
         products = Product.objects.all()
         categories = Category.objects.filter( is_sub= False)
 
-        paginator = Paginator(p , 4)
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-        products = page_obj
+        if category_slug:
+            category = Category.objects.get(slug = category_slug)
+            products = products.filter(category = category)
 
         search_form = SearchForm()
         if 'search' in request.GET:
-            products = Product.objects.all()
             search_form= SearchForm(request.GET)
             if search_form.is_valid():
                 cd = search_form.cleaned_data['search']
                 products = products.filter(title__icontains = cd)
 
-        if category_slug:
-            products = Product.objects.all()
-            category = Category.objects.get(slug = category_slug)
-            products = products.filter(category = category)
+        paginator = Paginator(products , 4)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        products = page_obj
             
         return render(request, 'base.html', {'product': page_obj ,'products': products , 'categories':categories , 'search_form': search_form })
-    
-
-    # def get(self, request):
-
-    #     p = Product.objects.all()
-
-    #     paginator = Paginator(p , 1)
-    #     page_number = request.GET.get('page')
-    #     page_obj = paginator.get_page(page_number)
-
-    #     return render(request, 'shop/product.html', {'product': page_obj })
-
-
-
