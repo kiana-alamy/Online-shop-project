c485460f293d19bf77e05da775a918a5b70d1718
kiana alamy <as.faraso.97@gmail.com>
2023-08-31 13:15:23 +0430

    Use the cache to see the list of products

--- online_shop/shop/api_views.py
+++ online_shop/shop/api_views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from .models import Category  ,Product
 from .serializers import CategorySerializer
 from rest_framework import status
-
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+# کش
 
 class CategoryListView(APIView):
     def get(self, request):
@@ -11,7 +13,9 @@ class CategoryListView(APIView):
         ser_data = CategorySerializer(instance=cat, many=True).data
         return Response(ser_data, status=status.HTTP_200_OK)
 
+# تایمش میشه بیشتر هم باشه
 class ProductListView(APIView):
+    @method_decorator(cache_page(180))
     def get(self, request):
         pro = Product.objects.all()
         ser_data = CategorySerializer(instance=pro, many=True).data
